f36fcf9c2f46dc11428a4f50e1a2ce4c533f14ef
jcnvj <154053407+jcnvj@users.noreply.github.com>
2023-12-18 09:21:32 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -34,8 +34,8 @@ def main():
         st.write(result)
 
     if st.button('초기화'):
-            num1 = st.number_input("당신의 키를 입력하세요.", key="height_input_1")
-            num2 = st.number_input("당신의 몸무게를 입력하세요.", key="weight_input_1")
+            num1 = st.number_input("당신의 키를 입력하세요.")
+            num2 = st.number_input("당신의 몸무게를 입력하세요.")
 
 
 if __name__ == "__main__":
